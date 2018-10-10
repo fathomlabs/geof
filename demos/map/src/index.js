17601fe3e86c1635b932df968e4d61fe5c782739
Rik Smith-Unna <rik@fathomlabs.io>
2018-10-10 11:28:47 +0100

    Another path fixup

--- demos/map/src/index.js
+++ demos/map/src/index.js
@@ -14,6 +14,6 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(require('./stores/map'))
 
 app.route('/', require('./views/main'))
-app.route('/*', require('./views/404'))
+app.route('/*', require('./views/main'))
 
 module.exports = app.mount('body')
