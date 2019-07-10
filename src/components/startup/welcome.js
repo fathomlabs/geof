fee46db1aabdc575f0b17c2f3749a27a639b9684
Rik Smith-Unna <rik@fathomlabs.io>
2019-07-10 14:03:39 +0100

    Reduce header size

--- src/components/startup/welcome.js
+++ src/components/startup/welcome.js
@@ -6,8 +6,8 @@ module.exports = function component(state, emit, opts) {
   var inner = html`
   
   <div>
-    <h2>Hello! Welcome to G.E.O.F. – the app that helps you and your family safely enjoy the healthy benefits of eating fish caught in Ontario.
-    </h2>
+    <h3>Hello! Welcome to G.E.O.F. – the app that helps you and your family safely enjoy the healthy benefits of eating fish caught in Ontario.
+    </h3>
     <p>Fresh-caught fish is a healthy food, packed with nutrients and heart-healthy fats. However, contaminants in the land and water can sometimes make their way into the fish that we eat. To reduce your exposure to these contaminants, try to choose smaller fish, avoid eating the organs of fish, and avoid eating the eggs of fatty fish such as salmon and trout. </p>
     <p>The G.E.O.F. app can also help you track the fish that you’re eating. This app uses data from the Ontario Ministry of the Environment, Conservation and Parks. As you add the fish that you catch and eat to the app, the app automatically calculates whether you are still safely within the monthly consumption guidelines, making it easier to ensure that you are safely enjoying all of the healthy benefits of eating fish! </p>
     <p>Let’s get started. Happy fishing! </p>
